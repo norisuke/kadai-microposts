58317a21b2986370fc5740b52fac4eef850fc937
norisuke <jpsandzigzaglife@gmail.com>
2019-06-16 13:21:23 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  
   validates :content, presence: true, length: { maximum: 255 }
+  
+  # お気に入り機能
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :user
 end
